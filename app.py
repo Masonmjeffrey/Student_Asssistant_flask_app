81bee4b15d8afea333d5c98c3553fa3d93d731e4
Masonmjeffrey <71951694+Masonmjeffrey@users.noreply.github.com>
2022-09-07 20:57:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<p>{}</p>'.format(escape(word.capitalize()))
