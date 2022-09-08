177c0c52d88acede58b16c87de5835181a05593b
Masonmjeffrey <71951694+Masonmjeffrey@users.noreply.github.com>
2022-09-07 21:22:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ def hello_world():
 @app.route('/capitalize/<word>/')
 def capitalize(word):
     return word.capitalize()
+
+@app.route('/add/<int:n1>/<int:n2>/')
+def add(n1, n2):
+    return n1+n2
+
+@app.route('/concat_nums/n1/n2/')
+def concat_nums(n1, n2):
+    return str(n1) + str(n2)
