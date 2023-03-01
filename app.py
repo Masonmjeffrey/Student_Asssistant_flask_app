04747274544cda9cf43b2e070c78c8149f719720
Jeffrey Mason <mason.m.jeffrey@gmail.com>
2023-03-01 15:02:50 -0700

    trying separate drop

--- app.py
+++ app.py
@@ -14,14 +14,22 @@ def index():
     conn = psycopg2.connect("postgres://sample_flask_db_user:7qN9gGyh8FcUmg99oDG8tdv4QHelRJMm@dpg-cfvrsul269v0ptnleuhg-a/sample_flask_db")
     return "it works"
 
+
+@app.route("/db_drop")
+def db4():
+    conn = psycopg2.connect("postgres://sample_flask_db_user:7qN9gGyh8FcUmg99oDG8tdv4QHelRJMm@dpg-cfvrsul269v0ptnleuhg-a/sample_flask_db")
+    cur = conn.cursor()
+    cur.execute ('''
+        DROP TABLE Basketball;
+    ''')
+    conn.commit()
+    conn.close()
+
+
 @app.route("/db_create_select")
 def db2():
     conn = psycopg2.connect("postgres://sample_flask_db_user:7qN9gGyh8FcUmg99oDG8tdv4QHelRJMm@dpg-cfvrsul269v0ptnleuhg-a/sample_flask_db")
     cur = conn.cursor()
-    cur.execute ('''
-        DROP TABLE IF EXISTS Basketball;
-    ''')
-    conn.commit()
     cur.execute('''
         CREATE TABLE IF NOT EXISTS Basketball (
         City varchar(255),
@@ -44,6 +52,7 @@ def db2():
     records = cur.fetchall()
     conn.close()
     return records
+
 
 @app.route("/db_select")
 def db3():
